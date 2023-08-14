bd79ea19e37e8c062b045d2675913286f50ba179
nachof948 <nachof948@gmail.com>
2023-08-14 11:09:35 -0300

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
 
   const [input, setInput] = useState('')
 
-  const agregarValor = (valor) =>{
-    if((valor === '+' || valor === '-' || valor === '*' || valor === '/')
-    && (input.endsWith('+') || input.endsWith('-') || input.endsWith('*') || input.endsWith('/'))){
-      setInput(input.slice(0, -1) + valor)
-    } else{
+  const agregarValor = (valor) =>{ /* Parámetro valor: Esta función acepta un parámetro valor, que representa el dígito o el operador que se debe agregar a la entrada actual */
+    if((valor === '+' || valor === '-' || valor === '*' || valor === '/') 
+    && (input.endsWith('+') || input.endsWith('-') || input.endsWith('*') || input.endsWith('/'))){ /*  El primer bloque if verifica si el valor es un operador (+, -, * o /) y si la entrada actual (input) ya termina con un operador. Esto se hace para evitar la entrada de operadores seguidos. */
+      
+      setInput(input.slice(0, -1) + valor) /* Se toma la entrada actual (input) y se elimina el último carácter utilizando el método slice(0, -1) para quitar el operador duplicado. Luego, se agrega el nuevo operador (valor) a la entrada corregida. En esencia, esto reemplaza el operador duplicado anterior por el nuevo operador. */
+    } 
+    else{
       setInput(input + valor)
     }
   }
@@ -23,7 +25,6 @@ function App() {
   return (
     <>
       <div className='app'>
-        <h1>Calculadora</h1>
         <div className='calculadora-contenedor'>
           <Pantalla input={input}/>
           <div className="fila">
